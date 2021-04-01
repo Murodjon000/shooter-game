0e59219514afbe2177eb79bcabc81245585c69c0
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-04-01 22:14:27 +0500

    add Game over scene

---
+++ src/js/SceneGameOver.js
@@ -0,0 +1,10 @@
+import Phaser from "phaser";
+class SceneGameOver extends Phaser.Scene {
+  constructor() {
+    super({ key: "SceneGameOver" });
+  }
+
+  create() {}
+}
+
+export default SceneGameOver;
