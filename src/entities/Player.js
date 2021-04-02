1edfff7483b971322ba71719a65aff40ccb3b5d3
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-04-02 21:29:51 +0500

    add setScore and onDestroy  methods

--- src/entities/Player.js
+++ src/entities/Player.js
@@ -1,5 +1,6 @@
 import Entity from "../js/Entities";
 import PlayerLaser from "./PlayerLaser";
+import { storeScores } from "../helpers/storage";
 
 class Player extends Entity {
   constructor(scene, x, y, key) {
@@ -9,6 +10,7 @@ class Player extends Entity {
     this.setData("isShooting", false);
     this.setData("timerShootDelay", 10);
     this.setData("timerShootTick", this.getData("timerShootDelay") - 1);
+    this.setData("score", 0);
   }
   moveUp() {
     this.body.velocity.y = -this.getData("speed");
@@ -24,6 +26,24 @@ class Player extends Entity {
 
   moveRight() {
     this.body.velocity.x = this.getData("speed");
+  }
+
+  onDestroy() {
+    this.scene.time.addEvent({
+      delay: 1000,
+      callback: function () {
+        this.scene.scene.start("SceneGameOver");
+      },
+      callbackScope: this,
+      loop: false,
+    });
+  }
+
+  setScore(value) {
+    if (!this.getData("isDead")) {
+      this.setData("score", this.getData("score") + value);
+      storeScores(this.getData("score"));
+    }
   }
 
   update() {
