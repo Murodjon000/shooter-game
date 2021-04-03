77adad43a4f152ffea652c297e87daae79cc8a37
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-04-03 23:03:33 +0500

    add top 5 scores list

--- src/js/SceneLeaderBoard.js
+++ src/js/SceneLeaderBoard.js
@@ -1,15 +1,12 @@
 import Phaser from "phaser";
 import ScrollingBackground from "../entities/ScrollingBackground";
+import { getData } from "../helpers/api";
 
-import { getLocalScore } from "../helpers/storage";
-import { getData, setData } from "../helpers/api";
+import sprBtnRestart from "../files/restrat.png";
 
-import sprBtnRestart from "../files/sprBtnRestart.png";
-import sprBtnRestartHover from "../files/sprBtnRestartHover.png";
-import sprBtnRestartDown from "../files/sprBtnRestartDown.png";
 import sprBg0 from "../files/sprBg0.png";
 import sprBg1 from "../files/sprBg1.png";
-
+import mainPage from "../files/main-page.png";
 import sndBtnOver from "../files/sndBtnOver.mp3";
 import sndBtnDown from "../files/sndBtnDown.mp3";
 
@@ -22,8 +19,7 @@ class SceneLeaderBoard extends Phaser.Scene {
     this.load.image("sprBg0", sprBg0);
     this.load.image("sprBg1", sprBg1);
     this.load.image("sprBtnRestart", sprBtnRestart);
-    this.load.image("sprBtnRestartHover", sprBtnRestartHover);
-    this.load.image("sprBtnRestartDown", sprBtnRestartDown);
+    this.load.image("mainPage", mainPage);
     this.load.audio("sndBtnOver", sndBtnOver);
     this.load.audio("sndBtnDown", sndBtnDown);
   }
@@ -35,15 +31,21 @@ class SceneLeaderBoard extends Phaser.Scene {
     };
 
     this.btnRestart = this.add.sprite(
-      this.game.config.width * 0.5,
-      this.game.config.height * 0.5,
+      this.game.config.width * 0.25,
+      this.game.config.height * 0.925,
       "sprBtnRestart"
+    );
+
+    this.btnMain = this.add.sprite(
+      this.game.config.width * 0.7,
+      this.game.config.height * 0.925,
+      "mainPage"
     );
 
     this.btnRestart.on(
       "pointerover",
       function () {
-        this.btnRestart.setTexture("sprBtnRestartHover");
+        this.btnRestart.setTexture("sprBtnRestart");
         this.sfx.btnOver.play();
       },
       this
@@ -55,7 +57,7 @@ class SceneLeaderBoard extends Phaser.Scene {
     this.btnRestart.on(
       "pointerdown",
       function () {
-        this.btnRestart.setTexture("sprBtnRestartDown");
+        this.btnRestart.setTexture("sprBtnRestart");
         this.sfx.btnDown.play();
       },
       this
@@ -68,24 +70,22 @@ class SceneLeaderBoard extends Phaser.Scene {
       },
       this
     );
-
-    this.scores = getLocalScore();
-    this.scoreScene = this.add.text(
-      this.game.config.width * 0.025,
-      this.game.config.height * 0.925,
-      `Score: ${this.scores[0]}`,
-      {
-        color: "#d0c600",
-        fontFamily: "sans-serif",
-        fontSize: "3vw",
-        lineHeight: 1.3,
-      }
+    this.btnMain.on(
+      "pointerup",
+      function () {
+        this.btnMain.setTexture("mainPage");
+        this.scene.start("SceneMainMenu");
+      },
+      this
     );
+
+    this.btnMain.setScale(0.4);
+    this.btnRestart.setScale(0.4);
 
     this.title = this.add.text(
       this.game.config.width * 0.5,
       128,
-      "SPACE SHOOTER",
+      "LEADERS BOARD",
       {
         fontFamily: "monospace",
         fontSize: 48,
@@ -96,6 +96,7 @@ class SceneLeaderBoard extends Phaser.Scene {
     );
     this.title.setOrigin(0.5);
     this.btnRestart.setInteractive();
+    this.btnMain.setInteractive();
 
     this.backgrounds = [];
     for (var i = 0; i < 5; i++) {
@@ -103,6 +104,39 @@ class SceneLeaderBoard extends Phaser.Scene {
       var key = keys[Phaser.Math.Between(0, keys.length - 1)];
       var bg = new ScrollingBackground(this, key, i * 10);
       this.backgrounds.push(bg);
+    }
+
+    this.getScores();
+  }
+
+  async getScores() {
+    try {
+      const data = await getData();
+      data.forEach((elem, index) => {
+        this.add.text(
+          this.game.config.width * 0.25,
+          this.game.config.height * 0.35 + index * 50,
+          `${index + 1}. ${elem.user}: ${elem.score}`,
+          {
+            color: "#d0c600",
+            fontFamily: "sans-serif",
+            fontSize: "3vw",
+            lineHeight: 1.3,
+          }
+        );
+      });
+    } catch {
+      this.add.text(
+        this.game.config.width * 0.35,
+        this.game.config.height * 0.35,
+        `Sorry scroe data is unable to get`,
+        {
+          color: "#d0c600",
+          fontFamily: "sans-serif",
+          fontSize: "1vw",
+          lineHeight: 1.3,
+        }
+      );
     }
   }
 
